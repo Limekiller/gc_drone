d18712f62c4fca6c4d460eea500daf9df58ed767
Bryce Yoder <me@bryceyoder.com>
2019-11-14 13:40:18 -0500

    Update and rename usb_example.ino to light_demo.ino

--- light_demo.ino
+++ light_demo.ino
@@ -4,18 +4,26 @@
 void setup() {
   Serial.begin(9600);
   pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
 void loop() {
   if(Serial.available() > 0) {
-    char data = Serial.read();
-    int volt = data;
-
+    char volt = Serial.read();
+    
     // Literally just read data from serial monitor and check if it's 0 or 1.
     // The trick is sending bytes from Python rather than strings. See the companion .py script for more
     if (volt == 0) {
+      digitalWrite(6, LOW);
+      digitalWrite(7, LOW);
+    } else if (volt == 1) {
+      digitalWrite(6, LOW);
+      digitalWrite(7, HIGH);
+    } else if (volt == 2) {
+      digitalWrite(6, HIGH);
       digitalWrite(7, LOW);
     } else {
+      digitalWrite(6, HIGH);
       digitalWrite(7, HIGH);
     }
   }
