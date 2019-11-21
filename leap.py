eb28f616abe73454812eafeb7f3e221dcedf3695
Bryce Yoder <me@bryceyoder.com>
2019-11-21 08:41:28 -0500

    Add files via upload

--- leap.py
+++ leap.py
@@ -15,19 +15,27 @@ listener = SampleListener()
 controller = Leap.Controller()
 controller.add_listener(listener)
 
-# port = serial.Serial("/dev/ttyUSB0", 9600)
-time.sleep(2)
+port = serial.Serial("/dev/ttyUSB0", 9600) # Connect to the Arduino through the correct serial port
+time.sleep(2) # Wait for connection
+
+# If the LEAP detects no hands in the frame for over 5 seconds, the drone will land.
+# We do this by comparing two time variables against each other, and a separate variable to tell if the timer is running or not.
+# These are initalized here.
 time_start = 0
+hands_off_time = time.time()
+timer_on = False
+
+# Variables for determining which state the drone is in
 flying = False
 start = False
 
-hands_off_time = time.time()
-timer_on = False
 
 direction = "stopped" # direction can equal up, down, left, right, hover
 avg_y_list = [0 for i in range(5)]
 avg_x_list = [0 for i in range(5)]
 avg_z_list = [0 for i in range(5)]
+
+velocity = 0
 
 while True:
 
@@ -37,22 +45,33 @@ while True:
 
     time.sleep(.05)
 
-    print direction
     if start:
         if direction == "takeOff" and not flying:
-            # port.write(struct.pack('>B', 1))
+            port.write(struct.pack('>B', 101))
             flying = True
             time.sleep(0.600)
-        #elif direction == "up":
-        #    # port.write(struct.pack('>B', 2))
-        #elif direction == "down":
-        #    # port.write(struct.pack('>B', 3))
-        #elif direction == "right":
-        #    # port.write(struct.pack('>B', 4))
-        #elif direction == "left":
-        #    # port.write(struct.pack('>B', 5))
-        #elif direction == "hover":
-        #    # port.write(struct.pack('>B', 0))
+        elif direction == "up":
+            port.write(struct.pack('>B', int(str(velocity)+str(2))))
+           # port.write(struct.pack('>B', 102))
+        elif direction == "down" or direction == "stopped":
+            port.write(struct.pack('>B', int(str(velocity)+str(3))))
+           # port.write(struct.pack('>B', 103))
+        elif direction == "right":
+            port.write(struct.pack('>B', int(str(velocity)+str(4))))
+           # port.write(struct.pack('>B', 104))
+        elif direction == "left":
+            port.write(struct.pack('>B', int(str(velocity)+str(5))))
+           # port.write(struct.pack('>B', 105))
+        elif direction == "forwards":
+            port.write(struct.pack('>B', int(str(velocity)+str(6))))
+           # port.write(struct.pack('>B', 106))
+        elif direction == "back":
+            port.write(struct.pack('>B', int(str(velocity)+str(7))))
+           # port.write(struct.pack('>B', 107))
+        elif direction == "hover":
+            port.write(struct.pack('>B', 100))
+
+        print direction
 
     # Collect information if there is a hand in view
     if len(frame.hands) == 0:
@@ -76,42 +95,32 @@ while True:
 
         hands = frame.hands
         timer_on = False
+        print hands[0].palm_position
 
-        # Determine direction on Y axis
-        avg_y_list.pop(0)
-        if hands[0].palm_velocity[1] > 50:
-            info_arr.append(1)
-            avg_y_list.append(1)
-        elif hands[0].palm_velocity[1] < -50:
-            info_arr.append(-1)
-            avg_y_list.append(-1)
+        #if -50 < hands[0].palm_position[0] < 50 and 100 < hands[0].palm_position[1] < 200 and -50 < hands[0].palm_position[2] < 50:
+        if hands[0].palm_position[0] < -50:
+            direction = 'left'
+            velocity = max(0, min(abs(int((hands[0].palm_position[0] + 50) / 5)) + 10, 20))
+        elif hands[0].palm_position[0] > 50:
+            direction = 'right'
+            velocity = max(0, min(abs(int((hands[0].palm_position[0] - 50) / 5)) + 10, 20))
+        elif hands[0].palm_position[2] < -50:
+            direction = 'forwards'
+            velocity = max(0, min(abs(int((hands[0].palm_position[2] + 50) / 5)) + 10, 20))
+        elif hands[0].palm_position[2] > 50:
+            direction = 'back'
+            velocity = max(0, min(abs(int((hands[0].palm_position[2] - 50) / 2)) + 10, 20))
+        elif hands[0].palm_position[1] < 150:
+            direction = 'down'
+            velocity = max(0, min(abs(int((hands[0].palm_position[1] - 150) / 5)) + 10, 20))
+        elif hands[0].palm_position[1] > 300:
+            direction = 'up'
+            velocity = max(0, min(abs(int((hands[0].palm_position[1] - 300) / 5)) + 10, 20))
         else:
-            info_arr.append(0)
-            avg_y_list.append(0)
+            direction = 'hover'
+            velocity = 10
 
-        # Determine direction on X axis
-        avg_x_list.pop(0)
-        if hands[0].palm_velocity[0] > 50:
-            info_arr.append(1)
-            avg_x_list.append(1)
-        elif hands[0].palm_velocity[0] < -50:
-            info_arr.append(-1)
-            avg_x_list.append(-1)
-        else:
-            info_arr.append(0)
-            avg_x_list.append(0)
-
-        # Determine direction on Z axis
-        avg_z_list.pop(0)
-        if hands[0].palm_velocity[2] > 50:
-            info_arr.append(-1)
-            avg_z_list.append(-1)
-        elif hands[0].palm_velocity[2] < -50:
-            info_arr.append(1)
-            avg_z_list.append(1)
-        else:
-            info_arr.append(0)
-            avg_z_list.append(0)
+        print velocity
 
         # Determine which way palm is facing
         hand_dir = hands[0].palm_normal.roll
@@ -124,45 +133,43 @@ while True:
         else:
             info_arr.append(2)
 
-        print info_arr
-        print avg_x_list
+       # # Detect left and right motion
+       # if avg_x_list.count(avg_x_list[0]) == 5:
+       #     if info_arr[1] == 1:
+       #         direction = 'right'
+       #         flying = True
+       #     elif info_arr[1] == -1:
+       #         direction = 'left'
+       #         flying = True
 
-        # Detect left and right motion
-        if avg_x_list.count(avg_x_list[0]) == 5:
-            if info_arr[1] == 1:
-                direction = 'right'
-                flying = True
-            elif info_arr[1] == -1:
-                direction = 'left'
-                flying = True
+       # # Detect back and forth motion
+       # if avg_z_list.count(avg_z_list[0]) == 5:
+       #     if info_arr[2] == 1:
+       #         direction = 'forward'
+       #         flying = True
+       #     elif info_arr[2] == -1:
+       #         direction = 'back'
+       #         flying = True
 
-        # Detect back and forth motion
-        if avg_z_list.count(avg_z_list[0]) == 5:
-            if info_arr[2] == 1:
-                direction = 'forward'
-                flying = True
-            elif info_arr[2] == -1:
-                direction = 'back'
-                flying = True
+       # # Detect up and down motion
+       # if avg_y_list.count(avg_y_list[0]) == 5:
+       #     if info_arr[0] == 1 and info_arr[3] == 2:
+       #         direction = 'up'
+       #         flying = True
+       #     elif info_arr[0] == -1:
+       #         direction = 'down'
+       #         flying = True
 
-        # Detect up and down motion
-        if avg_y_list.count(avg_y_list[0]) == 5:
-            if info_arr[0] == 1:
-                direction = 'up'
-                flying = True
-            elif info_arr[0] == -1:
-                direction = 'down'
-                flying = True
-
-        if info_arr[3] == 2:
-            direction = "hover"
+       # if info_arr[3] == 2:
+       #     direction = "hover"
 
         if not flying:
             start = True
             direction = "takeOff"
 
         if hands[0].grab_strength == 1:
-            direction = "stopped"
+            direction = "flip"
+            time.sleep(0.400)
 
     else:
         direction = "hover"
